92da9eb39fc9475af1dc39a814cb978685a6a160
Ashuu12 <89252476+Ashuu12@users.noreply.github.com>
2022-05-05 22:34:57 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function insert(num)
+{
+  document.form.textview.value = document.form.textview.value+num;
+}
+
+function back()
+{
+  var exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0,exp.length-1);
+}
+
+function clean(){
+  document.form.textview.value = "";
+}
+
+function equal()
+{
+  var exp = document.form.textview.value;
+  if(exp)
+  {
+    document.form.textview.value = eval(exp);
+  }
+}
